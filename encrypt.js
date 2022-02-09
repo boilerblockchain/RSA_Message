388753d3e2626b275b7f4cfca9b168c848c2316c
Mohammed Alnamkani <60770895+Alnamkani@users.noreply.github.com>
2022-02-08 20:50:43 -0500

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -1,12 +1,12 @@
 const NodeRSA = require('node-rsa');
 
 // TODO: fill in someone's public key
-const publicKey = "MEgCQQDmAE8A41d2QjigQjrEaMo0URPnspMHPedYhf2mvINYSFA76sffs2wAX+NR8qC2EI4CXaaCiw5fpEYSFPfUem1vAgMBAAE=";
+const publicKey = "publicKey goes here";
 
 const key = new NodeRSA(publicKey, 'pkcs1-public-pem');
 
 // TODO: add a message to encrypt
-const message = "hi Jason";
+const message = "";
 const encrypted = key.encrypt(message, 'base64');
 
 console.log(encrypted);
